alexvorxx/zink-xlib-termux

commit 2de95773b9cc366c4d4b1339a993e31f8812b2f2

panvk: Kill panvk_private.h
Explicitly include the headers we need in the various .{c,h} files and
remove panvk_private.h.

Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>
Acked-by: Erik Faye-Lund <erik.faye-lund@collabora.com>
Reviewed-by: Mary Guillemard <mary.guillemard@collabora.com>
Reviewed-by: Rebecca Mckeever <rebecca.mckeever@collabora.com>
Part-of: <https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/28170>

src/panfrost/vulkan/panvk_android.c
@@ -6,8 +6,6 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include "panvk_private.h"
-
 #include <hardware/hardware.h>
 #include <hardware/hwvulkan.h>
 #include <vulkan/vk_icd.h>

src/panfrost/vulkan/meson.build
@@ -46,7 +46,6 @@ libpanvk_files = files(
   'panvk_mempool.c',
   'panvk_physical_device.c',
   'panvk_pipeline.c',
-  'panvk_private.h',
   'panvk_priv_bo.c',
   'panvk_query.c',
   'panvk_shader.c',

src/panfrost/vulkan/panvk_buffer.c
@@ -6,7 +6,9 @@
 #include "panvk_buffer.h"
 #include "panvk_device.h"
 #include "panvk_device_memory.h"
-#include "panvk_private.h"
+#include "panvk_entrypoints.h"
+
+#include "vk_log.h"
 
 VKAPI_ATTR VkDeviceAddress VKAPI_CALL
 panvk_GetBufferDeviceAddress(VkDevice _device,

src/panfrost/vulkan/panvk_cmd_buffer.c
@@ -30,15 +30,17 @@
 #include "panvk_buffer.h"
 #include "panvk_cmd_pool.h"
 #include "panvk_device.h"
+#include "panvk_entrypoints.h"
 #include "panvk_physical_device.h"
 #include "panvk_pipeline.h"
 #include "panvk_pipeline_layout.h"
-#include "panvk_private.h"
 
 #include "pan_encoder.h"
 #include "pan_props.h"
 
 #include "util/rounding.h"
+
+#include "vk_alloc.h"
 #include "vk_format.h"
 #include "vk_framebuffer.h"
 

src/panfrost/vulkan/panvk_cmd_buffer.h
@@ -16,7 +16,6 @@
 #include "panvk_macros.h"
 #include "panvk_mempool.h"
 #include "panvk_pipeline.h"
-#include "panvk_private.h"
 #include "panvk_shader.h"
 
 #include "pan_jc.h"

src/panfrost/vulkan/panvk_cmd_pool.c
@@ -11,7 +11,10 @@
 
 #include "panvk_cmd_pool.h"
 #include "panvk_device.h"
-#include "panvk_private.h"
+#include "panvk_entrypoints.h"
+
+#include "vk_alloc.h"
+#include "vk_log.h"
 
 VKAPI_ATTR VkResult VKAPI_CALL
 panvk_CreateCommandPool(VkDevice _device,

src/panfrost/vulkan/panvk_descriptor_set.c
@@ -27,8 +27,9 @@
 
 #include "panvk_descriptor_set.h"
 #include "panvk_device.h"
+#include "panvk_entrypoints.h"
+#include "panvk_pipeline_layout.h"
 #include "panvk_priv_bo.h"
-#include "panvk_private.h"
 
 #include <assert.h>
 #include <fcntl.h>
@@ -36,8 +37,9 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "util/mesa-sha1.h"
+#include "vk_alloc.h"
 #include "vk_descriptors.h"
+#include "vk_log.h"
 #include "vk_util.h"
 
 VKAPI_ATTR VkResult VKAPI_CALL

src/panfrost/vulkan/panvk_device_memory.c
@@ -9,7 +9,9 @@
 
 #include "panvk_device.h"
 #include "panvk_device_memory.h"
-#include "panvk_private.h"
+#include "panvk_entrypoints.h"
+
+#include "vk_log.h"
 
 VKAPI_ATTR VkResult VKAPI_CALL
 panvk_AllocateMemory(VkDevice _device,

waydroid/android_external_mesa3d

commit 7d2ec14a5de1deb3f1cbf03d78863d3ecea00010

panvk: Add missing headers to android platform
Fixes: 2de95773b9c ("panvk: Kill panvk_private.h")

src/panfrost/vulkan/panvk_android.c
@@ -6,11 +6,15 @@
  * SPDX-License-Identifier: MIT
  */
 
+#include <stdlib.h>
+
 #include <hardware/hardware.h>
 #include <hardware/hwvulkan.h>
 #include <vulkan/vk_icd.h>
 
 #include "util/log.h"
+
+#include "panvk_entrypoints.h"
 
 static int panvk_hal_open(const struct hw_module_t *mod, const char *id,
                           struct hw_device_t **dev);