a10ef00440774e42afd8f03316a50f72ff3b163a
Alessandro Astone <ales.astone@gmail.com>
2026-04-26 00:12:31 +0200

    radv: Support explicit DRM format modifier from android gralloc
    Forwarded: https://gitlab.freedesktop.org/mesa/mesa/-/merge_requests/41183
    Closes: https://github.com/waydroid/android_external_minigbm/issues/2
    Closes: https://github.com/waydroid/waydroid/issues/1882
    Closes: https://github.com/waydroid/waydroid/issues/1964
    Closes: https://github.com/waydroid/waydroid/issues/1967

--- src/amd/vulkan/radv_android.c
+++ src/amd/vulkan/radv_android.c
@@ -23,6 +23,8 @@
 #include "vk_log.h"
 #include "vk_util.h"
 
+#define MAX_PLANES 4
+
 #if DETECT_OS_ANDROID
 
 VkResult
@@ -97,6 +99,16 @@ radv_image_from_gralloc(VkDevice device_h, const VkImageCreateInfo *base_info,
    };
 
    updated_base_info.pNext = &external_memory_info;
+
+   VkImageDrmFormatModifierExplicitCreateInfoEXT mod_info;
+   VkSubresourceLayout layouts[MAX_PLANES];
+   assert(vk_find_struct_const(base_info->pNext, NATIVE_BUFFER_ANDROID));
+   result = vk_android_get_anb_layout(base_info, &mod_info, layouts, MAX_PLANES);
+   if (result == VK_SUCCESS) {
+      mod_info.pNext = updated_base_info.pNext;
+      updated_base_info.tiling = VK_IMAGE_TILING_DRM_FORMAT_MODIFIER_EXT;
+      updated_base_info.pNext = &mod_info;
+   }
 
    result = radv_image_create(device_h,
                               &(struct radv_image_create_info){
@@ -415,7 +427,18 @@ radv_import_ahb_memory(struct radv_device *device, struct radv_device_memory *me
 
       struct radv_image_create_info create_info = {.no_metadata_planes = true, .bo_metadata = &metadata};
 
-      result = radv_image_create_layout(device, create_info, NULL, NULL, mem->image);
+      VkImageDrmFormatModifierExplicitCreateInfoEXT *mod_info_p = NULL;
+      VkImageDrmFormatModifierExplicitCreateInfoEXT mod_info;
+      VkSubresourceLayout layouts[MAX_PLANES];
+      result = vk_android_get_ahb_layout(info->buffer, &mod_info, layouts, MAX_PLANES);
+      if (result == VK_SUCCESS) {
+         for (unsigned plane = 0; plane < mem->image->plane_count; ++plane) {
+            mem->image->planes[plane].surface.modifier = mod_info.drmFormatModifier;
+         }
+         mod_info_p = &mod_info;
+      }
+
+      result = radv_image_create_layout(device, create_info, mod_info_p, NULL, mem->image);
       if (result != VK_SUCCESS) {
          radv_bo_destroy(device, NULL, mem->bo);
          mem->bo = NULL;
